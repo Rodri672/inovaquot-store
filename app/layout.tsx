5ea5525780001655883f93678248b671e6d923f6
TarzaAngolano <rbarral016@gmail.com>
2024-05-26 22:55:34 +0100

    loja completa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,34 @@
-import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
-import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Urbanist } from 'next/font/google'
 
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
 
-const font = Urbanist({ subsets: ["latin"] });
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Store",
-  description: "Store",
-};
+  title: 'Store',
+  description: 'Store',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ModalProvider />
+        <ToasterProvider />
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,28 @@
-import Container from "@/components/ui/container";
 import Link from "next/link";
+
+import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
-const Navbar = () => {
+import NavbarActions from "@/components/navbar-actions";
+import getCategories from "@/actions/get-categories";
+
+export const revalidate = 0;
+
+const Navbar = async () => {
+    const categories = await getCategories();
+
     return (
-        <div className=" border-b">
+        <div className="border-b">
             <Container>
-                <div className=" relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                        <p className=" font-bold text-xl">LOJA</p>
+                        <p className="font-bold text-xl">STORE</p>
                     </Link>
-                    <MainNav/>
+                    <MainNav data={categories} />
+                    <NavbarActions />
                 </div>
             </Container>
-
         </div>
     );
 }
-
+ 
 export default Navbar;
