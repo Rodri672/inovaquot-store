1bdd29771c56a6e7b2b1b40ef14e71f47c76500b
TarzaAngolano <rbarral016@gmail.com>
2024-06-05 16:51:36 +0100

    dwadwadwadwadawwdawd

--- app/(routes)/category/[categoryId]/client-category-page.tsx
+++ app/(routes)/category/[categoryId]/client-category-page.tsx
@@ -44,13 +44,14 @@ const ClientCategoryPage: React.FC<ClientCategoryPageProps> = ({
   return (
 
     <div className="mt-6 lg:col-span-4 lg:mt-0">
-      <div onClick={OpenNavbar} >
+      <div key="button"  onClick={OpenNavbar} >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
         </svg>
       </div>
 
       {products.length === 0 && <NoResults />}
+
       {!isOpen && (
         <div key="col" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {sortedProducts.map((item) => (
@@ -61,11 +62,12 @@ const ClientCategoryPage: React.FC<ClientCategoryPageProps> = ({
           ))}
         </div>
       )}
+
       {isOpen && (
         <div  key="list"  className="grid grid-cols-1">
           {sortedProducts.map((item) => (
             <div className=" w-max ">
-              <ProductCard key={item.id + 'list'} data={item} />
+              <ProductCard key={item.name} data={item} />
             </div>
 
           ))}
